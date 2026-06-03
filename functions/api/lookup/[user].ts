e680723461753070cef832004e6685ea804dcdfe
Neia <neeia2718@gmail.com>
2026-06-03 14:34:06 -0700

    modify function response

--- functions/api/lookup/[user].ts
+++ functions/api/lookup/[user].ts
@@ -1,8 +1,13 @@
+import { UserData } from "@/types/external/runelite";
 import type { EventContext } from "@cloudflare/workers-types";
 
 export async function onRequest(
   context: EventContext<unknown, "user", Record<string, unknown>>,
 ) {
   const remote = `https://sync.runescape.wiki/runelite/player/${context.params.user}/STANDARD`;
-  return await fetch(remote);
+  const response = await fetch(remote);
+  const data = (await response.json()) as UserData;
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
 }

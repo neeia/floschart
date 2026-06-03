a58eb27cb13efa8f96b064b61b3e4f2684627268
Neia <neeia2718@gmail.com>
2026-06-03 14:25:35 -0700

    move functions (again)

--- functions/api/lookup/[user].ts
+++ functions/api/lookup/[user].ts
@@ -1,6 +1,6 @@
 import type { EventContext } from "@cloudflare/workers-types";
 
-export default async function onRequest(
+export async function onRequest(
   context: EventContext<unknown, "user", Record<string, unknown>>,
 ) {
   const remote = `https://sync.runescape.wiki/runelite/player/${context.params.user}/STANDARD`;
